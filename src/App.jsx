7b2d3a4982dbecacfb65985b07f666c8af278730
IntepeZafer <intepezafer@gmail.com>
2023-09-13 03:26:27 +0300

    Abouth Page Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Abouth from './Companents/Abouth'
 import Headers from './Companents/Headers'
 function App() {
   return (
     <div className='container'>
       <Headers></Headers>
+      <Abouth></Abouth>
     </div>
   )
 }
---
+++ src/Companents/Abouth.jsx
@@ -0,0 +1,17 @@
+function Abouth() {
+    return ( 
+        <div className="AbouthContent">
+            <div className="contentSide">
+                <div className="leftSide"><img src="../../public/el-salvador-dali-889515_1280.jpg" alt="" /></div>
+                <div className="rightSide">
+                    <div className="rightSideText">
+                        <div className="title"><h3>Abouth</h3></div>
+                    <div className="text"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut molestias magni velit pariatur temporibus vitae architecto dolore recusandae, fugit sed explicabo modi eligendi exercitationem autem libero enim, qui odio ratione dicta impedit ad porro. Eaque quaerat voluptas veritatis nulla, consequatur quisquam perspiciatis minima, neque, fugit beatae vero a provident. Perferendis, et! Natus deleniti inventore eum! Placeat debitis, mollitia neque enim cum sequi ad vero pariatur porro, harum asperiores sint quo alias! Unde maiores, officiis molestiae nemo quisquam aliquid vitae, suscipit debitis ex, cum iste asperiores repudiandae fuga ipsa? Veniam aliquid natus nesciunt aut totam blanditiis pariatur consequuntur, fuga excepturi quos velit molestiae alias suscipit a eligendi nisi officia. Eos, totam ipsa accusamus commodi iure, aspernatur quasi mollitia magni obcaecati ratione animi, amet explicabo facilis voluptatem laudantium nostrum! Sequi exercitationem maxime sapiente odit beatae tempora laborum, nulla dolorum libero cum ea omnis soluta quisquam eos hic veniam quasi dolore deleniti optio?</p></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default Abouth;
